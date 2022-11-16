879140a5b2fed91c6201f9da8c59cfe5c532e31a
Gurleen <gurleen2@myseneca.ca>
2022-11-16 00:16:18 -0500

    initial comit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,17 @@
-import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate'
+import './ExpenseItem.css'
+import Card from './Card'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+   
     return (
-        <div className="expense-item">
-            <div>March 28th 2021</div>
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$259</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
