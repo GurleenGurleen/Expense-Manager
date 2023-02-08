84a573dbd49e08713dfd60005e22e2ce17dc5f13
Gurleen <gurleen2@myseneca.ca>
2023-02-07 20:05:38 -0500

    Second Commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate.js';
+import Card from '../UI/Card.js';
+function ExpenseItem(props) {
+
+
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </Card>
+    )
+}
+
+export default ExpenseItem;
