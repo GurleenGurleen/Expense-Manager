847b2040f31eecfcf2e27a9b21ff70dbdfce6c9a
Gurleen <gurleen2@myseneca.ca>
2023-02-17 06:11:49 -0500

    6th Commit: Expense Filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
+import ExpenseFilter from './ExpenseFilter'
 
 const Expenses = (props) => {
     return (
+
+
         <Card className="expenses">
+            <ExpenseFilter />
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
@@ -32,6 +36,7 @@ const Expenses = (props) => {
                 date={props.items[3].date} />
 
         </Card>
+
     )
 }
 
