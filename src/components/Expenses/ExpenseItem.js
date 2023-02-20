f1b952e94e2fd390f4a7d3551bc3fe79df32487f
Gurleen <gurleen2@myseneca.ca>
2023-02-20 04:25:24 -0500

    Adding and Displaying expense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate.js';
 import Card from '../UI/Card.js';
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
 
-
-    const ExpenseItem = () => {
-
-        setTitle('Updated!');
-        console.log(title);
-    }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={ExpenseItem}>Change Title</button>
+
         </Card>
     )
 }
